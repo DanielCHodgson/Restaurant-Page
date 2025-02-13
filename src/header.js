7a33b99a5cf582e8d4e44b39c8cf904f6c6ef725
Dan H <daniel.c.hodgson@address.com>
2025-02-13 08:18:09 +0000

    Updated to programatically add buttons to dom

--- src/header.js
+++ src/header.js
@@ -2,28 +2,36 @@ import logo from "./assets/images/logo.png";
 
 const header = () => {
 
-const header = document.querySelector(".header");
-const nav = header.querySelector(".nav");
+    const header = document.querySelector(".header");
+    const nav = header.querySelector(".nav");
 
 
-render();
+    render();
 
 
-function render() {
+    function render() {
 
-    const logoWrapper = document.createElement("div") ;
-    logoWrapper.classList.add("logo-wrapper");
-    const logoImg = document.createElement("img");
-    logoImg.id = "logo";
-    logoImg.src = logo;
-    logoWrapper.append(logoImg);
+        const logoWrapper = document.createElement("div");
+        logoWrapper.classList.add("logo-wrapper");
+        const logoImg = document.createElement("img");
+        logoImg.id = "logo";
+        logoImg.src = logo;
+        logoWrapper.append(logoImg);
 
-    const heading = document.createElement("h1");
-    heading.textContent = "饗宴";
-    logoWrapper.append(heading);
+        const heading = document.createElement("h1");
+        heading.textContent = "饗宴";
+        logoWrapper.append(heading);
 
-    header.prepend(logoWrapper);
-}
+        const buttonNames = ["MENUS", "RESERVATIONS", "EVENTS"];
+
+        for (let i = 0; i < 3; i++) {
+            const button = document.createElement("button");
+            button.textContent = buttonNames[i];
+            nav.appendChild(button);
+        }
+
+        header.prepend(logoWrapper);
+    }
 
 }
 
